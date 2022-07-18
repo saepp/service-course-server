7f54876c6d224915c6e248987d3659bd73c5be13
Septian Adhitya <dsrisk911@gmail.com>
2022-07-18 18:31:43 +0700

    create model Lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lesson extends Model
 {
-    use HasFactory;
+    protected $table = 'lessons';
+
+    protected $fillable = [
+        'name', 'video', 'chapter_id'
+    ];
 }
