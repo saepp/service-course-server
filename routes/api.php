0f0fa24f72297670c46d937c1f065e2243cb0886
Septian Adhitya <dsrisk911@gmail.com>
2022-07-21 00:08:04 +0700

    create update ChapterController

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,4 @@ Route::get('courses', [CourseController::class, 'index']);
 Route::delete('courses/{id}', [CourseController::class, 'destroy']);
 
 Route::post('chapters', [ChapterController::class, 'create']);
+Route::put('chapters/{id}', [ChapterController::class, 'update']);
