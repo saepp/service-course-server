8d54066801aef422de7396925984844f0f3e3b38
Septian Adhitya <dsrisk911@gmail.com>
2022-07-20 19:38:30 +0700

    create GetAll CourseController

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,4 @@ Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
 
 Route::post('courses', [CourseController::class, 'create']);
 Route::put('courses/{id}', [CourseController::class, 'update']);
+Route::get('courses', [CourseController::class, 'index']);
