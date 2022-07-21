3437cb9a1b393be0da8e7078d11860efa624ca2b
Septian Adhitya <dsrisk911@gmail.com>
2022-07-21 17:16:28 +0700

    create LessonController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LessonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MentorController;
@@ -33,3 +34,5 @@ Route::put('chapters/{id}', [ChapterController::class, 'update']);
 Route::get('chapters', [ChapterController::class, 'index']);
 Route::get('chapters/{id}', [ChapterController::class, 'show']);
 Route::delete('chapters/{id}', [ChapterController::class, 'destroy']);
+
+Route::post('lessons', [LessonController::class, 'create']);
