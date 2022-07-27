69f090713f08fb101ea2e4a2a346ab8f3012213a
Septian Adhitya <dsrisk911@gmail.com>
2022-07-27 12:50:52 +0700

    add destroy ReviewController

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -96,4 +96,22 @@ class ReviewController extends Controller
             'data' => $review
         ]);
     }
+
+    public function destroy($id)
+    {
+        $review = Review::find($id);
+        if (!$review) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'review not found'
+            ], 404);
+        }
+
+        $review->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'review deleted'
+        ]);
+    }
 }
