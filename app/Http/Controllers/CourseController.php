0b8a07d22696497c0c9fba409b9f66712df1294b
Septian Adhitya <dsrisk911@gmail.com>
2022-07-27 13:03:08 +0700

    integrate models

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -35,7 +35,7 @@ class CourseController extends Controller
 
     public function show($id)
     {
-        $course = Course::find($id);
+        $course = Course::with('chapters.lessons')->with('mentor')->with('images')->find($id);
         if (!$course) {
             return response()->json([
                 'status' => 'error',
